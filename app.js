e966bd4d2dfd3d3cd51789757d4959beccb860a9
Muhammad Thoriq Dzulfiqar <muhthoriqdzulfiqar@gmail.com>
2024-10-28 14:36:54 +0700

    membuat perintah make file

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-const fs = require("node:fs")
-const readline = require('node:readline');
+const fs = require("node:fs");
+const readline = require("node:readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const app = {}
+const app = {};
 
 // contoh script pembuatan folder
- app.makeFolder = () => {
-    rl.question("Masukan Nama Folder : ",(folderName) => {
-        fs.mkdir(__dirname + `/${folderName}`,() => {
-            console.log("success created new folder");
-            
-        })
-        rl.close()
-    })
-} 
+app.makeFolder = () => {
+  rl.question("Masukan Nama Folder : ", (folderName) => {
+    fs.mkdir(__dirname + `/${folderName}`, () => {
+      console.log("success created new folder");
+    });
+    rl.close();
+  });
+};
 
-// To Do : lanjutkan pembuatan logic disini 
+// To Do : lanjutkan pembuatan logic disini
 
+// * **make-file** : digunakan untuk membuat sebuah file
+app.makeFile = () => {
+  rl.question("Masukan Nama Folder : ", (folder) => {
+    rl.question("Masukan Nama File : ", (file) => {
+      rl.question("Masukan Nama Extension : ", (ext) => {
+        console.log(folder, file, ext);
+        fs.writeFileSync(`${folder}/${file}.${ext}`, "");
+        rl.close();
+      });
+    });
+  });
+};
 
-module.exports = app
+module.exports = app;
