099ad952ec74f0c1a990e591bd5dfdba09149810
Muhammad Thoriq Dzulfiqar <muhthoriqdzulfiqar@gmail.com>
2024-10-28 14:33:41 +0700

    mengedit command di index.js

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ const args = process.argv.slice(2);
 const command = args[0];
 
 switch (command) {
-    case "make-folder":
-        app.makeFolder()
-        break;
+  case "make-folder":
+    app.makeFolder();
+    break;
 
-    default:
-        throw Error("Invalid command")
-        break;
+  case "make-file":
+    app.makeFile();
+    break;
+
+  case "ext-sorter":
+    app.extSorter();
+    break;
+
+  case "read-folder":
+    app.readFolder();
+    break;
+
+  case "read-file":
+    app.readFile();
+    break;
+
+  default:
+    throw Error("Invalid command");
+    break;
 }
- 
-
