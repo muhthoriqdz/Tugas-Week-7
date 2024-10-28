3eaf7f88d8f82e6a628ba259b3a77cec07b24b8b
Muhammad Thoriq Dzulfiqar <muhthoriqdzulfiqar@gmail.com>
2024-10-28 20:18:36 +0700

    hasil tugas akhir tanpa output di branch main

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ const args = process.argv.slice(2);
 const command = args[0];
 
 switch (command) {
-    case "make-folder":
-        app.makeFolder()
-        break;
+  case "make-folder":
+    app.makeFolder();
+    break;
 
-    default:
-        throw Error("Invalid command")
-        break;
+  default:
+    throw Error("Invalid command");
+    break;
 }
- 
-
